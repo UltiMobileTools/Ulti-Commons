61c127b3724b7889ec85de9924ca66d883b48ed5
wuph <1131960938@qq.com>
2024-07-31 11:32:50 +0800

    全局可变

--- commons/src/main/kotlin/com/ultimobiletools/commons/admob/AdsConstant.kt
+++ commons/src/main/kotlin/com/ultimobiletools/commons/admob/AdsConstant.kt
@@ -2,16 +2,15 @@ package com.ultimobiletools.commons.admob
 
 object AdsConstant {
 
-    const val TEST_DEVICE_HASHED_ID = "3d0db8cd-3586-4e3f-b09e-f1540d0c1696"
+    val TEST_DEVICE_HASHED_ID = "3d0db8cd-3586-4e3f-b09e-f1540d0c1696"
 
+    var AD_OPEN_UNIT_ID = ""
 
-    const val AD_OPEN_UNIT_ID = ""
+    val AD_BANNER_UNIT_ID = ""
 
-    const val AD_BANNER_UNIT_ID = ""
+    val AD_INTERSTITIALAD_UNIT_ID = ""
 
-    const val AD_INTERSTITIALAD_UNIT_ID = ""
-
-    const val AD_REWARDED_UNIT_ID = ""
+    val AD_REWARDED_UNIT_ID = ""
 
 
 
