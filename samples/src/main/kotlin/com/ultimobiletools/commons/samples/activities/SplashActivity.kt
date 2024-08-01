64c7f0f372f577d07597aa0daf93d4659ac83e32
wuph <1131960938@qq.com>
2024-08-01 12:45:27 +0800

    优化

--- samples/src/main/kotlin/com/ultimobiletools/commons/samples/activities/SplashActivity.kt
+++ samples/src/main/kotlin/com/ultimobiletools/commons/samples/activities/SplashActivity.kt
@@ -2,12 +2,36 @@ package com.ultimobiletools.commons.samples.activities
 
 import android.animation.Animator
 import android.content.Intent
-import com.ultimobiletools.commons.activities.BaseAdActivity
 import com.ultimobiletools.commons.activities.BaseSplashActivity
 import com.ultimobiletools.commons.samples.R
 
-class SplashActivity : BaseAdActivity() {
-    override fun goActivity() {
+class SplashActivity : BaseSplashActivity() {
+    override fun backgroundColor(): Int {
+        return resources.getColor(com.ultimobiletools.commons.R.color.md_grey_white);
+    }
+
+    override fun animation(): Int {
+        return R.raw.res_splash_ani;
+    }
+
+    override fun initActivity() {
+//        startActivity(Intent(this, MainActivity::class.java))
+//        finish()
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onAnimationEnd(p0: Animator) {
+        System.currentTimeMillis();
+    }
+
+    override fun onAnimationRepeat(p0: Animator) {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
