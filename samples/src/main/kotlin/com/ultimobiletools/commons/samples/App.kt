3c516e9df286618d1db75d507b83fa6aec4349c7
wuph <1131960938@qq.com>
2024-08-07 14:06:46 +0800

    新增hisavana

--- samples/src/main/kotlin/com/ultimobiletools/commons/samples/App.kt
+++ samples/src/main/kotlin/com/ultimobiletools/commons/samples/App.kt
@@ -2,16 +2,11 @@ package com.ultimobiletools.commons.samples
 
 import android.app.Application
 import com.github.ajalt.reprint.core.Reprint
-import com.ultimobiletools.commons.hisavana.AdManager
-import com.ultimobiletools.commons.hisavana.AdManagerInterface
-import com.ultimobiletools.commons.hisavana.TAdsConstant
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Reprint.initialize(this)
-        TAdsConstant.APPID = "240807hdOpgzf8"
-        TAdsConstant.AD_OPEN_UNIT_ID = "240807o4bavR1a"
     }
 
 }
